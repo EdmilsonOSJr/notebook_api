b067417b97a5c483a4af605cefa828569a472c16
EdmilsonOSJr <edmilsonosjr@gmail.com>
2022-02-19 19:40:26 -0300

    fix links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :kinds
-  resources :contacts
+  resources :contacts do 
+    resource :kind, only: [:show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
