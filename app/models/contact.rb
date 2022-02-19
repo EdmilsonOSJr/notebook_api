3f2d3f06eececcb3185f070fea4abcc8213e94bc
EdmilsonOSJr <edmilsonosjr@gmail.com>
2022-02-18 23:07:24 -0300

    add parametro kind no json do contato

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,19 @@
 class Contact < ApplicationRecord
-    belongs_to :kind
+    belongs_to :kind, optional: true
 
-    def author
-        "Edmilson"
-    end
-
-    def as_json(options={})
-        super(methods: :author, root: true)
-    end
+    #def author
+    #    "Edmilson"
+    #end
+#
+    #def kind_description
+    #    self.kind.description
+    #end
+#
+    #def as_json(options={})
+    #    super(
+    #        methods: [ :kind_description, :author ], 
+    #        root: true,
+    #        include: { kind: {only: :description} }
+    #    )
+    #end
 end
