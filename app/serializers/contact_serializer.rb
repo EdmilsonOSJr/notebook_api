c5d5b88a527419c2712827e69ccdda545e9494cf
EdmilsonOSJr <edmilsonosjr@gmail.com>
2022-02-19 17:50:51 -0300

    add links

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,20 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
-  belongs_to :kind
+  belongs_to :kind do 
+    link(:related){ kind_url(object.kind.id)}
+  end
   has_many :phones
   has_one :address
+
+#link(:self){ contact_url(object.id)}
+
+
+meta do 
+  {
+    author: "Edmilson"
+  }
+end
 
 def attributes(*args)
   h = super(*args)
