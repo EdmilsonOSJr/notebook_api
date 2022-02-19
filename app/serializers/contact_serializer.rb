f271ee24d36919cafd40fe95709af9fede404009
EdmilsonOSJr <edmilsonosjr@gmail.com>
2022-02-19 16:00:03 -0300

    add atributos do contact e do kind

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,3 +1,13 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :email, :birthdate
+
+
+def attributes(*args)
+  h = super(*args)
+
+  ##h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+  h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
+  h
+end
+
 end
