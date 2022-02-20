acbdbcfcbb42003c3d442ad59bf01b088a6abe25
EdmilsonOSJr <edmilsonosjr@gmail.com>
2022-02-19 22:47:43 -0300

    add CRUD com AMS para o phones

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -1,15 +1,44 @@
 class PhonesController < ApplicationController
-    before_action :set_phones
+    before_action :set_contact
 
+    def destroy 
+        phone = Phone.find(phones_params[:id])
+        phone.destroy
+    end
+
+
+    def create 
+        @contact.phones << Phone.new(phones_params)
+
+        if @contact.save
+            render json: @contact.phones, status: :created, localtion: contact_phones_url(@contact)
+        else
+            render json: @contact.erros, status: :unprocessable_entity 
+        end
+    end
+
+    def update
+        phone = Phone.find(phones_params[:id])
+
+        if phone.update(phones_params)
+            render json: @contact.phones 
+        else
+            render json: @contact.erros, status: :unprocessable_entity 
+        end
+    end
 
     def show
-        render json: @phones
-      end
+        render json: @contact.phones
+    end
 
 private
     # Use callbacks to share common setup or constraints between actions.
-    def set_phones
-        @phones = Contact.find(params[:contact_id]).phones
+    def set_contact
+        @contact = Contact.find(params[:contact_id])
+    end
+
+    def phones_params
+        ActiveModelSerializers::Deserialization.jsonapi_parse(params)
     end
 
 end
